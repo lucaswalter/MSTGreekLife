97a6fd18ab27893f899af9c861210789fb54b471
BuildTools <unconfigured@null.spigotmc.org>
2016-05-03 04:07:55 -0500

    Cleanup

--- MSTGreekLife/Controllers/PartyController.cs
+++ MSTGreekLife/Controllers/PartyController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using MSTGreekLife.DAL;
